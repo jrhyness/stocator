1aa01d2620978b889231db83bc47a4d22b1046dc
Jim Rhyness <jrhyness@ca.ibm.com>
2018-03-29 16:35:06 -0400

    Revert "The last one worked, but trying something else."
    This reverts commit 9b52e062755dc7bbc027c1671762845c819f1d97.

--- src/main/java/com/ibm/stocator/fs/swift/http/SwiftConnectionManager.java
+++ src/main/java/com/ibm/stocator/fs/swift/http/SwiftConnectionManager.java
@@ -34,8 +34,12 @@ import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.SocketConfig;
+import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -70,7 +74,26 @@ public class SwiftConnectionManager {
   public SwiftConnectionManager(ConnectionConfiguration connectionConfigurationT) {
     connectionConfiguration = connectionConfigurationT;
 
-    connectionPool = new PoolingHttpClientConnectionManager();
+    SSLConnectionSocketFactory myFactory = null;
+    try {
+      SSLContext sslContext = SSLContexts.custom()
+                                         .build();
+
+      myFactory = new SSLConnectionSocketFactory(
+          sslContext,
+          new String[]{"TLSv1.2"},
+          null,
+          SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+
+    } catch (Exception e) {
+      LOG.trace("Caught exception ... good enough for now");
+    }
+
+    connectionPool = new PoolingHttpClientConnectionManager(
+        RegistryBuilder.<ConnectionSocketFactory>create()
+                       .register("https",
+                                 myFactory)
+                       .build());
     LOG.trace(
         "SwiftConnectionManager: setDefaultMaxPerRoute {}",
         connectionConfiguration.getMaxPerRoute()
@@ -187,21 +210,13 @@ public class SwiftConnectionManager {
   public CloseableHttpClient createHttpConnection() {
     LOG.trace("HTTP build new connection based on connection pool -- JR");
 
-    try {
-      final SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-      sslContext.init(null, null, null);
-      CloseableHttpClient httpclient = HttpClients.custom()
+    CloseableHttpClient httpclient = HttpClients.custom()
                                                 .setRetryHandler(getRetryHandler())
                                                 .setConnectionManager(connectionPool)
                                                 .setDefaultRequestConfig(rConfig)
                                                 .setKeepAliveStrategy(myStrategy)
-                                                .setSSLContext(sslContext)
                                                 .build();
-      LOG.trace("HTTP created connection based on connection pool");
-      return httpclient;
-    } catch (Exception e) {
-      LOG.trace("Caught something");
-      return null;
-    }
+    LOG.trace("HTTP created connection based on connection pool");
+    return httpclient;
   }
 }
