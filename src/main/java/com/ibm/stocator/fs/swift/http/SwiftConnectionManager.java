ec48c37f656c8499441e9cc4294b182f7fb58c74
Jim Rhyness <jrhyness@ca.ibm.com>
2018-03-29 13:05:23 -0400

    Missed ;

--- src/main/java/com/ibm/stocator/fs/swift/http/SwiftConnectionManager.java
+++ src/main/java/com/ibm/stocator/fs/swift/http/SwiftConnectionManager.java
@@ -36,7 +36,7 @@ import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
